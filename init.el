4821c2469f224d9023df340b08c04ef2ab1df8c4
Alex Butler <lxbtlr@pm.me>
2026-01-23 16:25:53 -0600

    WE GOT OXOCARBON IN THIS JAWN

--- init.el
+++ init.el
@@ -250,3 +250,11 @@
   (add-to-list 'completion-at-point-functions #'cape-elisp-symbol))
 
 (advice-add 'eglot-completion-at-point :around #'cape-wrap-buster)
+(use-package doom-themes
+  :ensure t
+  :config
+  (setq doom-themes-enable-bold t    ; if nil, bold is universally disabled
+	doom-theme 'doom-oxocarbon
+        doom-themes-enable-italic t))
+
+(load-theme 'doom-oxocarbon t)
