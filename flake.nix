a0be37d30629f50c606c952101642fed762db2ad
Alex Butler <lxbtlr@pm.me>
2026-01-27 13:34:16 -0600

    trying something

--- flake.nix
+++ flake.nix
@@ -1,19 +1,43 @@
 {
-  description = "experimental emacs flake";
+  description = "My Emacs Binary Flake";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
   };
 
-outputs = { self, nixpkgs }:
+  outputs = { self, nixpkgs }: 
     let
-      system = "x86_64-linux"; 
+      system = "x86_64-linux";
       pkgs = nixpkgs.legacyPackages.${system};
-    in
-    {
-      packages.${system}.default = pkgs.writeShellScriptBin "launch-emacs" ''
-        # --init-directory set location of conf files
-        ${pkgs.emacs}/bin/emacs -nw --init-directory $(pwd) "$@"
-      '';
+    in {
+      # Just export the raw Emacs package (e.g., emacs-gtk or emacs-pgtk)
+      packages.${system}.default = pkgs.emacs-pgtk; 
+      
+      # We export the path to this directory so Home Manager can find your init.el
+      lib.configPath = ./.; 
     };
 }
+
+
+#
+#{
+#  description = "experimental emacs flake";
+#
+#  inputs = {
+#    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+#  };
+#
+#outputs = { self, nixpkgs }:
+#    let
+#      system = "x86_64-linux"; 
+#      pkgs = nixpkgs.legacyPackages.${system};
+#    in
+#    {
+#      packages.${system}.default = pkgs.writeShellScriptBin "launch-emacs" ''
+#        # --init-directory set location of conf files
+#        ${pkgs.emacs}/bin/emacs -nw --init-directory $(pwd) "$@"
+#      '';
+#    };
+#}
+
+
